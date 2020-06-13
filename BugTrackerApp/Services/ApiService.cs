72cf3a37cdb1ff5e2693b9451ca8948d9ff5266c
Dawid Osuchowski <dawiosu469@student.polsl.pl>
2020-06-13 18:14:01 +0200

    Create BugTrackerApp, with working login/register screen

---
+++ BugTrackerApp/Services/ApiService.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+
+namespace BugTrackerApp.Services
+{
+    public static class ApiService
+    {
+        public static IApiService apiService;
+        static string baseUrl = "https://bugtrackerapi20200605232913.azurewebsites.net/api";
+        public static IApiService GetApiService()
+        {
+            apiService = RestService.For<IApiService>(baseUrl);
+            return apiService;
+        }
+    }
+}
---
+++ BugTrackerApp/Services/IApiService.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+using BugTrackerApp.Models;
+using Java.Lang;
+using Refit;
+
+namespace BugTrackerApp.Services
+{
+    public interface IApiService
+    {
+        [Post("/auth/login")]
+        Task<AuthToken> PostLogin([Body] AuthAccountDetails loginDetails);
+
+        [Post("/auth/register")]
+        Task<AuthId> PostRegister([Body] AuthAccountDetails registerDetails);
+    }
+}
